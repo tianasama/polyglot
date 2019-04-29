81d4843bb6068fbceb22606f75cf05525f967d8f
tianasama <tianaattride@gmail.coM>
2019-04-29 19:42:16 -0400

    Add initial site files

---
+++ precache-manifest.02ddc7a19a1d9f2d719280a4ab4f0998.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9443581fc127e73cd3c",
+    "url": "/polyglot/static/css/main.16f52c8f.chunk.css"
+  },
+  {
+    "revision": "f9443581fc127e73cd3c",
+    "url": "/polyglot/static/js/main.f27a7edb.chunk.js"
+  },
+  {
+    "revision": "5861ac10d5268e5ed8ae",
+    "url": "/polyglot/static/js/runtime~main.2411d4c6.js"
+  },
+  {
+    "revision": "0eb6bf7c7f6d2ce85f72",
+    "url": "/polyglot/static/js/2.d57301aa.chunk.js"
+  },
+  {
+    "revision": "f93c69f254e46f64129f952b2960a0dd",
+    "url": "/polyglot/static/media/title-planet.f93c69f2.gif"
+  },
+  {
+    "revision": "ff19269bc19bb45a3136fc9d479b94dc",
+    "url": "/polyglot/static/media/cloud2-slow.ff19269b.gif"
+  },
+  {
+    "revision": "064f2af443c0aeb66e462b447fbd061e",
+    "url": "/polyglot/static/media/world.064f2af4.png"
+  },
+  {
+    "revision": "87793b5ae64c70e307bb6497f04f6253",
+    "url": "/polyglot/static/media/moderate.87793b5a.png"
+  },
+  {
+    "revision": "57ded46e789247b56720d9fcd5cf6ffe",
+    "url": "/polyglot/static/media/difficult.57ded46e.png"
+  },
+  {
+    "revision": "1ad30f0f183b4d4b5d4d930c6f130a3f",
+    "url": "/polyglot/static/media/advanced.1ad30f0f.png"
+  },
+  {
+    "revision": "fcca8e5e888f0caae4f36f76eb9433ab",
+    "url": "/polyglot/static/media/planet-icon.fcca8e5e.png"
+  },
+  {
+    "revision": "072013314d73af731fd419c23beec310",
+    "url": "/polyglot/index.html"
+  }
+];
